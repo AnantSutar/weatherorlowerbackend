032c0c6c2b0c746fd3fb815b7bb1d7139e500aef
Anant Sutar <ananthbksutar@gmail.com>
2026-04-01 09:02:45 +1100

    change PORT

--- index.js
+++ index.js
@@ -74,6 +74,8 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
